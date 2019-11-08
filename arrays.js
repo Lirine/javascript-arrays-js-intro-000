2c17930b12d5ec4cb7c540253fcfa00c0e372517
modern-integer-2602 <iloikov@gmail.com>
2019-11-08 17:06:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,8 +19,6 @@ function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
   return chocolateBars;
 }
 
-function accessElementInArray(abc,def){
-    abc = [98,54,78];
-    def=1;
-    return abc[def];
+function accessElementInArray(array,index){
+    return array[index];
 }
