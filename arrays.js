6baebc4b259769c1f42077361550308e9185148a
modern-integer-2602 <iloikov@gmail.com>
2019-11-08 17:03:27 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,26 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+//var candyString = "foo"
+
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return [candyString, ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, candyString){
+  return [...chocolateBars, candyString];
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
+
+function accessElementInArray(abc,def){
+    abc = [98,54,78];
+    def=1;
+    return abc[def];
+}
